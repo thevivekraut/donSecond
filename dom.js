384e24f5b3fe23d24c7115d9d86cb885795bdcb8
thevivekraut <vraut2306@gmail.com>
2022-10-14 11:16:36 +0530

    donSecond

---
+++ dom.js
@@ -0,0 +1,5 @@
+var list = document.getElementsByClassName('list-group-item');
+list[2].style.backgroundColor = 'yellow'
+for(var i=0; i<list.length; i++){
+    list[i].style.fontWeight = 'bold';
+}
